3483322a9ba7004e2b611192b281565a7127dbd3
iqrarasool04 <iqrarasool45@gmail.com>
2023-09-19 20:29:30 +0500

    task3

---
+++ task3.py
@@ -0,0 +1,74 @@
+def levDistance(str1, str2):
+    # creating a 2d array
+    matrix = [[0] * (len(str2) + 1) for row in range(len(str1) + 1)]
+        
+    #initializing array
+    for row in range(len(str1)+1):
+        matrix[row][0] = row
+    for col in range(len(str2)+1):
+        matrix[0][col] = col
+    
+    #finding distance
+    for row in range(1,len(str1)+1):
+        for col in range(1,len(str2)+1):
+            if str1[row-1]==str2[col-1]:
+                cost = 0
+            else:
+                cost = 1
+            matrix[row][col] = min(matrix[row-1][col],
+                                   matrix[row][col-1],
+                                   matrix[row-1][col-1])+cost
+    
+    # printing array
+    for i in matrix:
+        for x in i:
+            print(x,end=" ")
+        print()
+
+    return matrix
+
+def calculateOperations(matrix,words1,words2):
+    #calculating number of operations
+    i = len(words1)
+    j = len(words2)
+    operations = {"deletions":0, "insertions":0, "substitutions":0}
+    while i>0 and j>0:
+        if (words1[i-1] == words2[j-1]):
+            i-=1
+            j-=1
+        else:
+            if (matrix[i][j] == matrix[i-1][j]+1):
+                operations["deletions"]+=1
+                i-=1
+            elif (matrix[i][j] == matrix[i][j-1]+1):
+                operations["insertions"]+=1
+                j-=1
+            else:
+                operations["substitutions"]+=1
+                i-=1
+                j-=1
+    
+    return operations
+
+file1 = open("reference.txt","r")
+file2 = open("hypothesis.txt","r")
+
+str1 = file1.read()
+str2 = file2.read()
+
+w1=str1.split()
+w2=str2.split()
+#removing common words
+commonWords = ["the","of","and","a","be","this","there","an","been","some"]
+words1 = [word for word in w1 if word not in commonWords]
+words2 = [word for word in w2 if word not in commonWords]
+
+levMatrix = levDistance(words1,words2)
+distance = levMatrix[len(words1)][len(words2)]
+print("Levenshtein/Edit distance between the two strings is ",distance)
+
+operations = calculateOperations(levMatrix,words1,words2)
+print("Number of deletions are ",operations["deletions"])
+print("Number of insertions are ",operations["insertions"])
+print("Number of substitutions are ",operations["substitutions"])
+    
